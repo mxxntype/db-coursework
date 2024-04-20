d7d6f3e681d8400005ccc5a474d0dd376ed66cf5
mxxntype <59417007+mxxntype@users.noreply.github.com>
2024-04-20 09:50:27 +0300

    feat: Initial commit

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+    description = "";
+
+    inputs = {
+        # SECTION: Core inputs.
+        nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+
+        # SECTION: Nix libraries.
+        # Nix flake framework.
+        snowfall-lib = {
+            url = "github:snowfallorg/lib";
+            inputs.nixpkgs.follows = "nixpkgs";
+        };
+    };
+
+    outputs = inputs: inputs.snowfall-lib.mkFlake {
+        inherit inputs;
+        src = ./.;
+
+        # Snowfall Lib configuration.
+        snowfall.namespace = "aeon";
+    };
+}
---
+++ shells/default/default.nix
@@ -0,0 +1,12 @@
+{
+    pkgs,
+    ...
+}:
+
+pkgs.mkShell {
+    NIX_CONFIG = "extra-experimental-features = nix-command flakes repl-flake";
+    buildInputs = with pkgs; [
+        postgresql
+        glade
+    ];
+}
