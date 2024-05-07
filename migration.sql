6b194887865208bd5a37c434c043a983524b9a44
mxxntype <59417007+mxxntype@users.noreply.github.com>
2024-05-08 02:30:49 +0300

    fix(migration): Avoid race condition by handling an exception

--- migration.sql
+++ migration.sql
@@ -38,20 +38,13 @@ CREATE TABLE IF NOT EXISTS "ratings" (
 );
 
 -- A function for safely creating a ROLE, without causing exceptions.
---
--- SAFETY NOTE:
--- This function contains a race condition n the tiny time frame between looking
--- up the role and creating it. If a concurrent transaction creates the role in
--- between we get an exception after all. In most workloads, that will never
--- happen as creating roles is a rare operation carried out by an admin.
 CREATE OR REPLACE FUNCTION create_role_safe(rolename NAME, passwd VARCHAR) RETURNS TEXT AS
 $$ BEGIN
-    IF NOT EXISTS (SELECT 1 FROM pg_roles WHERE rolname = rolename) THEN
-        EXECUTE format('CREATE ROLE %I WITH LOGIN PASSWORD %L', rolename, passwd);
-        RETURN 'CREATE ROLE';
-    ELSE
+    EXECUTE format('CREATE ROLE %I WITH LOGIN PASSWORD %L', rolename, passwd);
+    RETURN 'CREATE ROLE';
+EXCEPTION
+    WHEN duplicate_object THEN
         RETURN format('ROLE "%I" ALREADY EXISTS', rolename);
-    END IF;
 END;
 $$ LANGUAGE plpgsql;
 
