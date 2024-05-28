f44dcfc364d243df80cb06ef84086bde60b15340
mxxntype <59417007+mxxntype@users.noreply.github.com>
2024-05-29 00:51:35 +0300

    chore(migrations/index): Reorder `CREATE` and `EXPLAIN`

--- migrations/20_index.sql
+++ migrations/20_index.sql
@@ -1,8 +1,7 @@
 CREATE INDEX ON authors (phone);
--- EXPLAIN SELECT * FROM authors WHERE phone = '7 (615) 255-2988';
-
 CREATE INDEX ON ratings USING HASH (rated_at);
--- EXPLAIN SELECT * FROM ratings WHERE rated_at = '2001-05-14 20:51:10';
-
 CREATE INDEX ON posts USING BRIN (created_at);
+
+-- EXPLAIN SELECT * FROM authors WHERE phone = '7 (615) 255-2988';
+-- EXPLAIN SELECT * FROM ratings WHERE rated_at = '2001-05-14 20:51:10';
 -- TODO
