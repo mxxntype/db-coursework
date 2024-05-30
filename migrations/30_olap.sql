2512d3123036ac5b306bb1578107577f2b6eb6f3
mxxntype <59417007+mxxntype@users.noreply.github.com>
2024-05-30 20:14:16 +0300

    fix(migrations): Fix `author` permissions

--- migrations/30_olap.sql
+++ migrations/30_olap.sql
@@ -1,20 +1,65 @@
 -- An OLAP table.
-CREATE TABLE post_logs (LIKE posts INCLUDING ALL);
-ALTER TABLE post_logs
-    ADD COLUMN event_type VARCHAR(10) NOT NULL,
-    ADD COLUMN event_time TIMESTAMP DEFAULT now(),
-    ADD COLUMN description TEXT;
+CREATE TABLE IF NOT EXISTS post_logs (
+    id            SERIAL    NOT NULL,
+    text          TEXT      NOT NULL,
+    title         VARCHAR   NOT NULL,
+    author_id     BIGINT    NOT NULL,
+    created_at    TIMESTAMP NOT NULL DEFAULT NOW(),
+    attachment_id BIGINT,
+    event_type    VARCHAR(10) NOT NULL,
+    event_time    TIMESTAMP DEFAULT now(),
+    description   TEXT,
+    PRIMARY KEY(id),
+    FOREIGN KEY(author_id) REFERENCES authors(id)
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION,
+    FOREIGN KEY(attachment_id) REFERENCES attachments(id)
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+);
 
 -- A procedure for logging post events in the OLAP table above.
 CREATE OR REPLACE FUNCTION log_post_event() RETURNS TRIGGER AS $$
 BEGIN
     CASE TG_OP
         WHEN 'INSERT' THEN
-            INSERT INTO post_logs SELECT NEW.*, 'INSERT', now(), 'New post with title: ' || NEW.title;
+            INSERT INTO
+                post_logs(text, title, author_id, created_at, attachment_id, event_type, event_time, description)
+            SELECT
+                NEW.text,
+                NEW.title,
+                NEW.author_id,
+                NEW.created_at,
+                NEW.attachment_id,
+                'INSERT',
+                now(),
+                'New post with title: ' || NEW.title;
+
         WHEN 'UPDATE' THEN
-            INSERT INTO post_logs SELECT NEW.*, 'UPDATE', now(), 'Post title updated from: ' || OLD.title || ' to: ' || NEW.title;
+            INSERT INTO
+                post_logs(text, title, author_id, created_at, attachment_id, event_type, event_time, description)
+            SELECT
+                NEW.text,
+                NEW.title,
+                NEW.author_id,
+                NEW.created_at,
+                NEW.attachment_id,
+                'UPDATE',
+                now(),
+                'Post title updated from: ' || OLD.title || ' to: ' || NEW.title;
+
         WHEN 'DELETE' THEN
-            INSERT INTO post_logs SELECT OLD.*, 'DELETE', now(), 'Deleted post with title: ' || OLD.title;
+            INSERT INTO
+                post_logs(text, title, author_id, created_at, attachment_id, event_type, event_time, description)
+            SELECT
+                OLD.text,
+                OLD.title,
+                OLD.author_id,
+                OLD.created_at,
+                OLD.attachment_id,
+                'DELETE',
+                now(),
+                'Deleted post with title: ' || OLD.title;
     END CASE;
     RETURN NULL;
 END;
--- migrations/80_roles.sql
+++ migrations/80_roles.sql
@@ -10,10 +10,16 @@ END; $$ LANGUAGE plpgsql;
 
 -- Create the reader (regular user) role and grant it necessary permissions.
 SELECT create_role_safe('reader', '12345');
-GRANT SELECT ON ALL TABLES IN SCHEMA public TO reader;
-GRANT INSERT ON ratings TO reader;
+GRANT USAGE, SELECT  ON ALL SEQUENCES IN SCHEMA public TO reader;
+GRANT SELECT         ON ALL TABLES IN SCHEMA public TO reader;
+GRANT SELECT, INSERT ON ratings TO reader;
 
 -- Create the author (admin user) role and grant it necessary permissions.
 SELECT create_role_safe('author', '12345');
-GRANT SELECT ON ALL TABLES IN SCHEMA public TO author;
-GRANT INSERT ON ALL TABLES IN SCHEMA public TO author;
+GRANT USAGE, SELECT  ON ALL SEQUENCES IN SCHEMA public TO author;
+GRANT SELECT         ON ALL TABLES IN SCHEMA public TO author;
+GRANT INSERT         ON ratings TO author;
+GRANT INSERT, UPDATE ON posts TO author;
+GRANT INSERT, UPDATE ON post_updates TO author;
+GRANT INSERT         ON post_logs TO author;
+ 
