24af8cc4a730ef4d8bec4ac3d4143b03f6c080f4
mxxntype <59417007+mxxntype@users.noreply.github.com>
2024-05-30 23:16:07 +0300

    fix(migrations): Fix the last `GIN` index on the `posts` table

--- migrations/20_index.sql
+++ migrations/20_index.sql
@@ -1,7 +1,12 @@
 CREATE INDEX ON authors (phone);
 CREATE INDEX ON ratings USING HASH (rated_at);
-CREATE INDEX ON posts USING BRIN (created_at);
+CREATE INDEX ON posts USING gin(to_tsvector('russian', text));
 
+-- For the `B-Tree` index:
 -- EXPLAIN SELECT * FROM authors WHERE phone = '7 (615) 255-2988';
+
+-- For the `HASH` index:
 -- EXPLAIN SELECT * FROM ratings WHERE rated_at = '2001-05-14 20:51:10';
--- TODO
+
+-- For the `GIN` index:
+-- EXPLAIN SELECT * FROM posts WHERE to_tsvector('russian', text) @@ to_tsquery('russian', 'слишком');
